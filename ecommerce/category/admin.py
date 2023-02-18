8f57a0d5087a15bb16613779bb1a79e2f07f54ee
Elghandour-eng <Abdelwahabhassan2000@gmail.com>
2023-02-18 12:52:45 +0200

    Add Category Model

---
+++ ecommerce/category/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Category
+# Register your models here.
+
+# Category Admin
+admin.site.register(Category)
---
+++ ecommerce/category/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+# Category Model
+class Category(models.Model):
+    
+    # Category Fields 
+    categoryName=models.CharField(max_length=50,unique=True) #Category Name {Unique=True}
+    categorySlug=models.CharField(max_length=50,unique=True) #Category Slug {Unique=True}
+    categoryDescription=models.TextField(max_length=200,blank=True) #Category Description
+    categoryImage=models.ImageField(upload_to='photos/categories',blank=True) #Category Image
+    
+    # Meta Class
+    class Meta:
+        verbose_name="Category" # Verbose Name is a single object to be displayed in the admin
+        verbose_name_plural="Categories" # Verbose Name Plural is a plural object to be displayed in the admin
+    
+    # default ordering
+    def __str__(self):
+        return self.categoryName
+    
