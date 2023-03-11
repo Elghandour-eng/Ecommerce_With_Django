940d9561b44e77d8a86d6e6a8dac4127a60a936c
Elghandour-eng <Abdelwahabhassan2000@gmail.com>
2023-03-11 13:46:44 +0200

    add stroe screen & category screen

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from . import views
 from django.conf import settings 
 from django.conf.urls.static import static
+from store import views as store_views
+from django.conf.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     #path("",views.home,name="home"),
-    path("",views.home,name="home")
+    path("",views.home,name="home"),
+    path("store/",include("store.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import Product 
+from category.models import Category
 
 # Create your views here.
+
+def store(request , category_slug=None):
+    products = None
+    categories = None
+    
+    if category_slug != None:
+        categories = get_object_or_404(Category, categorySlug=category_slug)
+        products = Product.objects.all().filter(product_category=categories, product_isavilabe=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(product_isavilabe=True)
+        product_count = products.count()
+
+    
+    context = {
+        "products": products,
+        "product_count": product_count,
+    }
+    return render(request, 'store/store.html', context)
