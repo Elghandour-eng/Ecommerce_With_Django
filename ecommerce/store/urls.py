7f3ef5f8b91cb3d82549804853ab800c316a656c
Elghandour-eng <Abdelwahabhassan2000@gmail.com>
2023-03-13 18:49:57 +0200

    add_product_details_view

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     
     path('', views.store, name='store'),
     path('<slug:category_slug>/', views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_details, name='product_details'),
  
    
 ] 
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 
 from .models import Product 
 from category.models import Category
+
 
 # Create your views here.
 
@@ -22,3 +23,19 @@ def store(request , category_slug=None):
         "product_count": product_count,
     }
     return render(request, 'store/store.html', context)
+
+
+def product_details(request,product_slug,category_slug):
+    
+    try:
+        single_product = Product.objects.get(product_slug=product_slug, product_category__categorySlug=category_slug)
+        in_stock = Product.objects.filter(product_isavilabe=True).count()
+    except Exception as e:
+        raise e
+    
+    context = {
+        "single_product": single_product,
+        "in_stock": in_stock,
+    }
+    return render(request, 'store/product_details.html', context)
+    
