7ee280a9e143af559d07039f6522d6705418f5b2
Elghandour-eng <Abdelwahabhassan2000@gmail.com>
2023-03-15 22:27:05 +0200

    add_cart_screen

---
+++ ecommerce/carts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+
+
+urlpatterns = [
+    path("",views.cart_home,name="carts"),
+ ]
--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart_home(request):
+    return render(request, "store/carts.html", {})
