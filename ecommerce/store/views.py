4a6d1e86fd86f7a23feef39312690b0a07847492
Elghandour-eng <Abdelwahabhassan2000@gmail.com>
2023-03-24 14:24:40 +0200

    check_if_in_cart

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render , get_object_or_404 
 from .models import Product 
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -30,12 +33,15 @@ def product_details(request,category_slug,product_slug):
     try:
         single_product = Product.objects.get(product_slug=product_slug, product_category__categorySlug=category_slug)
         in_stock = Product.objects.filter(product_isavilabe=True).count()
+        in_cart =CartItem.objects.filter(cart__cart_id=_cart_id(request),
+                                            product=single_product).exists()
     except Exception as e:
         raise e
     
     context = {
         "single_product": single_product,
         "in_stock": in_stock,
+        "in_cart": in_cart,
     }
     return render(request, 'store/product_details.html', context)
     
