2786d7158ad20ae103b3b96c46e9410079a20893
Elghandour-eng <Abdelwahabhassan2000@gmail.com>
2023-03-25 12:09:12 +0200

    add pagination logic in store

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
@@ -18,12 +19,16 @@ def store(request , category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(product_isavilabe=True)
+        paginator = Paginator(products ,6) # Show 25 contacts per page
+        page = request.GET.get("page") # Get the page number from the url
+        paged_products = paginator.get_page(page) # Get the page number from the url
         product_count = products.count()
 
     
     context = {
-        "products": products,
+        "products": paged_products,
         "product_count": product_count,
+        
     }
     return render(request, 'store/store.html', context)
 
